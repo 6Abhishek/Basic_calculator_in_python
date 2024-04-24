32352a7a0a876a0c3229a86cb06e79e568b2d468
abhi <brightlight052023@gmail.com>
2024-04-24 22:52:47 +0530

    Add comments

--- calculator.py
+++ calculator.py
@@ -1,18 +1,19 @@
+# Take input from the user
+num1 = int(input("Enter the 1st number:"))  # Read 1st number as an integer
+op = input("Enter the operator:")  # Read operator (+, -, *, /, %)
+num2 = int(input("Enter the 2nd number:"))  # Read 2nd number as an integer
 
-num1 = int(input("Enter the 1st number:"))
-op = input("Enter the operator:")
-num2 = int(input("Enter the 2nd number:"))
-
+# Perform the operation based on the operator
 match(op):
     case '+':
-        print(num1+num2)
+        print(num1 + num2)  # Addition
     case '-':
-        print(num1-num2)
+        print(num1 - num2)  # Subtraction
     case '*':
-        print(num1*num2)
+        print(num1 * num2)  # Multiplication
     case '/':
-        print(num1/num2)
+        print(num1 / num2)  # Division
     case '%':
-        print(num1%num2)
+        print(num1 % num2)  # Modulo
     case __:
-        print("Please Enter a Valid operator")
+        print("Please Enter a Valid operator")  # Invalid operator message
